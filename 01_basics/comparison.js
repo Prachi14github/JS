651fa99a45fdaceffd4094cb2a4146f79185422f
Prachi14github <115626786+Prachi14github@users.noreply.github.com>
2024-02-04 08:08:30 +0000

    COMPARISON BETN DATATYPES

---
+++ 01_basics/comparison.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+console.log(2>=1)
+console.log(2==1)
+console.log(2<1)
+console.log(2!=1)
+
+
+console.log("2" >1)
+console.log(null < 0)
+console.log(null <= 0)
+console.log(null == 0)
+
+// THESE TYPE OF COMPARISONS AND CONVERSIONS SHOULD BE AVOIDED
+
+// equality == and comparison </> signs work differently
+//comaprison converts null into a number, treating it as 0
+
+
+// ===
+console.log("2"=== 1)
--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -26,5 +26,38 @@ console.log(booleanIsLoggedIn)
 let SomeNumber=33
 
 let stringNumber= String(SomeNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+// ##############operations##############
+
+let value= 14
+let negValue = -value
+console.log(negValue)
+
+let str1="Hey"
+let str2="Prachi"
+let str3= str1 + str2
+//console.log(str3)
+
+
+//console.log("1"+2)
+//console.log(1+"2")
+
+//console.log("1"+"2")
+
+//console.log("1" +2 +2 )
+//console.log(1+2 +"2")
+
+console.log(+true)
+
+
+let num1 , num2 , num3
+
+num1=num2=num3=14+10
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
+
