c98f062a67a2d6a033e8896d7520be9768bfa89c
Prachi14github <115626786+Prachi14github@users.noreply.github.com>
2024-02-04 06:53:28 +0000

    daa

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3)  coz we are using node js and not browser
+
+console.log(10+14)
+
+console.log("Prachi")
+
+let name ="Shivam"
+let age =23
+let isLoggedIn =true
+
+//number =2 to the power 53
+//bigint
+//string can be ' or ""
+//boolean = true or false
+//null = standalone value
+//undefined = when variable has been defined but value isnt assigned
+//symbol = defines uniqueness
+
+
+
+
+//object
+
+console.log(typeof "Prachi") // tells y0u the type of an object
+
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 1445
+let acccountEmail = "prachi14@gmail.com"
+var accoundPassword = "1407"
+accountCity = "Mumbai"
+
+//accountId = 2
+
+
+acccountEmail="vbhjk@gmail.com"
+accoundPassword="prachi"
+accountCity="Pune"
+
+
+console.log (accountId);
+
+
+/*
+prefer not to use var coz of issue in block scope and functional scope
+*/
+
+
+console.table([accountId , acccountEmail , accoundPassword , accountCity])
