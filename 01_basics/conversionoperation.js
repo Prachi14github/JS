7ceb46f3cf8f4fe97cc7bb16e92d1fd99686e872
Prachi14github <115626786+Prachi14github@users.noreply.github.com>
2024-02-04 07:22:16 +0000

    conversion of datatype

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,30 @@
+let score ="Prachi"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//BASIC DATATYPE CONVERSION
+// "45" = 45
+//"45abc" = NaN
+// TRUE = 1 ; FALSE = 0
+
+
+let isLoggedIn = "prachi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//1 = true
+// ""= false
+//"prachi" = true
+
+
+let SomeNumber=33
+
+let stringNumber= String(SomeNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
